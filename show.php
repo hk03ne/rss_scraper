26c0a8ef2f0d609120dbad52e9cb69e8142ea111
Hiroya Komine <hk0mine@outlook.jp>
2019-08-22 00:40:18 +0900

    Fix table viewer

--- show.php
+++ show.php
@@ -3,6 +3,11 @@
 <head>
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 </head>
 <body>
 <h1>記事一覧</h1>
@@ -17,10 +22,16 @@ try {
   $stmt = $pdo->query($sql);
 
   while ($result = $stmt->fetch()) {
-    print($result['updated'] . " ");
-    print("<a href='" . $result['entry_url'] . "'>");
-    print($result['entry_title'] . "</a>");
-    print("<br>");
+    print("<div class=\"card\" style=\"width: 60rem;\">");
+    print("<div class=\"card-head\">");
+    print($result['updated']);
+    print("</div>");
+    print("<div class=\"card-body\">");
+    print("<h5 class=\"card-title\">" . $result['entry_title'] . "</h5>");
+    print("<p class=\"card-text\">" . $result['summary'] . "</p>");
+    print("<a href=\"" . $result['entry_url'] . "\" class=\"btn btn-primary\">Read more</a>");
+    print("</div>");
+    print("</div>");
   }
   print('<br>');
 

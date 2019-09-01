86a2d17463cdd6969baf4fcdd637fdf85b0c4b54
hiroya <kominehiroya@gmail.com>
2019-09-01 14:58:28 +0900

    Fix viewer

--- show.php
+++ show.php
@@ -7,10 +7,10 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
+<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<h1>記事一覧</h1>
+<h2 class="m-3">記事一覧</h2>
 <?php
 try {
   $db = "sqlite:production.sqlite3";
@@ -22,9 +22,9 @@ try {
   $stmt = $pdo->query($sql);
 
   while ($result = $stmt->fetch()) {
-    print("<div class=\"card\" style=\"width: 60rem;\">");
-    print("<div class=\"card-head\">");
-    print($result['updated']);
+    print("<div class=\"card m-3\">");
+    print("<div class=\"card-header text-muted\">");
+    print($result['site_title'] . " " . $result['updated']);
     print("</div>");
     print("<div class=\"card-body\">");
     print("<h5 class=\"card-title\">" . $result['entry_title'] . "</h5>");
